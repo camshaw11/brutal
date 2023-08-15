ce1f4dced67a1f81ec7d86b179aedc62b9585b3d
Cameron Shaw <cameron.shaw@commonledger.com>
2023-08-15 21:58:06 +0200

    add base vue setup

---
+++ brutal/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+</style>
